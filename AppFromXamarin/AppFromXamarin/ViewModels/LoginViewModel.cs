db601e9b4e13b7785a6f229c4dd8bd4676242b4e
Edwin Ospina <gy0v4nny182@gmail.com>
2023-07-24 15:53:07 -0500

    funcional v2
    APP funcional hasta la clase del 22 julio

--- AppFromXamarin/AppFromXamarin/ViewModels/LoginViewModel.cs
+++ AppFromXamarin/AppFromXamarin/ViewModels/LoginViewModel.cs
@@ -8,10 +8,74 @@ namespace AppFromXamarin.ViewModels
 {
     public class LoginViewModel : BaseViewModel
     {
+        private string _username;
+        private string _password;
+        private bool _showMessage;
+        private string _welcomeMessage;
+        private Color _messageColor;
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                if (_username != value)
+                {
+                    _username = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                if (_password != value)
+                {
+                    _password = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public Color MessageColor
+        {
+            get => _messageColor;
+            set
+            {
+                if (_messageColor != value)
+                {
+                    _messageColor = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public bool ShowMessage
+        {
+            get => _showMessage;
+            set
+            {
+                if (_showMessage != value)
+                {
+                    _showMessage = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public string WelcomeMessage
+        {
+            get => _welcomeMessage;
+            set
+            {
+                if (_welcomeMessage != value)
+                {
+                    _welcomeMessage = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
         public Command LoginCommand { get; }
 
-        public string UserName { get; set; }
-        public string Password { get; set; }
+        //public string UserName { get; set; }
 
         public LoginViewModel()
         {
@@ -21,7 +85,61 @@ namespace AppFromXamarin.ViewModels
         private async void OnLoginClicked(object obj)
         {
             // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
-            await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+            if (ValidateFiels())
+            {
+                //await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+                if (Username == "egop" && Password == "egop182")
+                {
+                    WelcomeMessage = "Inicio de sesión exitoso, " + "¡Bienvenido! " + Username + "!";
+                    MessageColor = Color.Black;
+                    await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+                }
+                else
+                {
+                    ShowMessage = true;
+                    MessageColor = Color.YellowGreen;
+                    WelcomeMessage = "Usuario o contraseña incorrectos";
+                }
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(Username) && string.IsNullOrEmpty(Password))
+                {
+                    ShowMessage = true;
+                    MessageColor = Color.Red;
+                    WelcomeMessage = "Los campos de Usuario y Password estan Vacios!!";
+                }
+                else
+                {
+                    if (string.IsNullOrEmpty(Username))
+                    {
+                        ShowMessage = true;
+                        MessageColor = Color.Red;
+                        WelcomeMessage = "El campo Usuario esta vacio!!";
+                    }
+                    else
+                    {
+                        if (string.IsNullOrEmpty(Password))
+                        {
+                            ShowMessage = true;
+                            MessageColor = Color.Red;
+                            WelcomeMessage = "El campo Password esta vacio!!";
+                        }
+                    }
+                }
+                
+            }
+        }
+        private bool ValidateFiels()
+        {
+            if (!string.IsNullOrEmpty(Username) && !string.IsNullOrEmpty(Password))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
--- AppFromXamarin/AppFromXamarin/obj/Debug/netstandard2.0/Views/LoginPage.xaml.g.cs
+++ AppFromXamarin/AppFromXamarin/obj/Debug/netstandard2.0/Views/LoginPage.xaml.g.cs
@@ -17,16 +17,8 @@ namespace AppFromXamarin.Views {
     public partial class LoginPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
-        private global::Xamarin.Forms.Entry USerNameEntry;
-        
-        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
-        private global::Xamarin.Forms.Entry PasswordEntry;
-        
-        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(LoginPage));
-            USerNameEntry = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "USerNameEntry");
-            PasswordEntry = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "PasswordEntry");
         }
     }
 }
