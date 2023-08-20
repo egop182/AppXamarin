beef89c8d8c8296016b7de95ee9310a2132190f3
Edwin Ospina <eospina@cfa.com.co>
2023-08-20 18:05:58 -0500

    Pantalla carga info
    Pantalla que carga informacion de productos ok

--- AppFromXamarin/AppFromXamarin/Services/IProductoService.cs
+++ AppFromXamarin/AppFromXamarin/Services/IProductoService.cs
@@ -2,12 +2,15 @@
 using AppFromXamarin.Data.Models;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Refit;
+using AppFromXamarin.Data.Dto;
 
 namespace AppFromXamarin.Services
 {
     public interface IProductoService
     {
         Task<List<Producto>> GetProductos();
+
         Task<Producto> GetProducto(int ProductoId);
     }
 }
